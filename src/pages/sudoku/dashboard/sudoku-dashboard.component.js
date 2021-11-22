19668c51a932b3750e53ca45192671c1c6cbc872
Robert-Rendell <rob_452@hotmail.co.uk>
2021-11-22 01:47:13 +0000

    Sudoku dashboard changes

--- src/pages/sudoku/dashboard/sudoku-dashboard.component.js
+++ src/pages/sudoku/dashboard/sudoku-dashboard.component.js
@@ -17,13 +17,13 @@ class SudokuDashboardComponent extends React.Component {
     return (
       <div id="sudoku-dashboard">
         <h1>Sudoku!</h1>
-        <hr/>
-        <h2>Play Sudoku (Gradual work in progress)</h2>
         <b>Progress Updates:</b>
         <p>
           - 16/11/21 @ 14.43 - Developed endpoint for user to submit partial/completed sudokus and have it validated.<br/>
           - 15/11/21 @ 15.53 - Sudoku loaded from AWS DynamoDB but the UI needs more dev work to play it
         </p>
+        <hr/>
+        <h2>Play Sudoku (Gradual work in progress)</h2>
         <Link to="/sudoku/play/0">Play a sudoku</Link>
         <br/>
         <h2>Generate Sudoku (coming soon)</h2>
