ad22adff90c4e8a2b21d0ddb2a9beb50a0551184
Robert-Rendell <rob_452@hotmail.co.uk>
2021-12-01 10:45:02 +0000

    tech test graphs for uni data collecting

--- src/pages/technical-tests/291121/uni-data-visualiser.component.js
+++ src/pages/technical-tests/291121/uni-data-visualiser.component.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import Highcharts from 'highcharts'
 import HighchartsReact from 'highcharts-react-official'
+import Table from 'react-bootstrap/Table';
 import axios from 'axios';
 import config from '../../../config';
-
+import './uni-data-visualiser.component.css';
 class UniDataVisualiserComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = { 
       date: new Date(),
-      options: {
+      bestUnisForSubjectGraph: {
         title: {
           text: 'My chart'
         },
@@ -18,16 +19,17 @@ class UniDataVisualiserComponent extends React.Component {
         }]
       }
     };
+    this.bestUnisForSubjectOptions = this.bestUnisForSubjectOptions.bind(this);
   }
 
   /**
    * Object the back end returns:
    * 
    * rawData: {
-      submissions: string;
-      institutions: string;
+      submissions;
+      institutions;
     };
-    bestUnisForSubject: string[],
+    bestUnisForSubject: [],
     submissionsPerYear: string[],
     listOfSubjectsAndWhereToStudy: string[];
     covid19CasesPerInstitution: string[];
@@ -37,15 +39,94 @@ class UniDataVisualiserComponent extends React.Component {
       headers: {'Content-Type': 'application/json'},
     }).then((response) => {
       console.log(response);
+      if (response.data.bestUnisForSubject) {
+        this.setState({
+          bestUnisForSubjectGraph: this.bestUnisForSubjectOptions(response.data.bestUnisForSubject),
+          listOfSubjectsAndWhereToStudy: response.data.listOfSubjectsAndWhereToStudy,
+        });
+      }
     });
+  }
+
+  bestUnisForSubjectOptions(bestUnisForSubjectData) {
+    return { chart: {
+      type: 'bar'
+    },
+    title: {
+        text: 'Best Unis for Subject'
+    },
+    subtitle: {
+        text: 'Based on the average student rating from questionnaire submissions'
+    },
+    xAxis: {
+        enabled: false,
+        categories: bestUnisForSubjectData.xAxis,
+        title: {
+            text: null
+        }
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'Average Student Rating',
+            align: 'high'
+        },
+        labels: {
+            overflow: 'justify'
+        }
+    },
+    tooltip: {
+        valueSuffix: ' millions'
+    },
+    plotOptions: {
+        bar: {
+            dataLabels: {
+                enabled: true
+            }
+        }
+    },
+    legend: {
+        layout: 'vertical',
+        align: 'right',
+        verticalAlign: 'top',
+        x: -40,
+        y: 80,
+        floating: true,
+        borderWidth: 1,
+        backgroundColor:
+            Highcharts.defaultOptions.legend.backgroundColor || '#FFFFFF',
+        shadow: true
+    },
+    credits: {
+        enabled: false
+    },
+    series: bestUnisForSubjectData.series};
   }
 
   render() {
     return (
-      <HighchartsReact
-      highcharts={Highcharts}
-      options={this.state.options}
-    />
+      <div>
+        <h2>List of subjects and where to study</h2>
+        <Table>
+        <tbody>
+          <tr>
+            {this.state.listOfSubjectsAndWhereToStudy?.map((value, index) => (
+              <td key={index}>{value.split(":")[0]}</td>
+            ))}
+          </tr>
+          <tr>
+            {this.state.listOfSubjectsAndWhereToStudy?.map((value, index) => (
+              <td key={`${index}-2`}>{value.split(":")[1]}</td>
+            ))}
+          </tr>
+        </tbody>
+        </Table>
+        <hr/>
+        <HighchartsReact
+          highcharts={Highcharts}
+          options={this.state.bestUnisForSubjectGraph}
+        />
+      </div>
     )
   }
 }
