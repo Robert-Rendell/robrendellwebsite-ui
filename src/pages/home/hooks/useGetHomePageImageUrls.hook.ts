b49dd54414ffc2c65d539174f075794e8dd1ffd6
Robert-Rendell <robert.rendell@timeshighereducation.com>
2022-10-27 10:26:35 +0100

    Home - moved from component style to functional hooks

---
+++ src/pages/home/hooks/useGetHomePageImageUrls.hook.ts
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import { useCallback } from 'react';
+import { config } from '../../../config';
+
+const addHomePageImage = (imgUrl: string) => {
+  return "<img class=\"home-page-img\" src=\"" + imgUrl + "\"/ onClick={activateLasers}>";
+}
+
+export const useGetHomePageImageUrls = () => useCallback(() => {
+  axios.get(config.backend,
+    { headers: {'Content-Type': 'application/json'}}
+  ).then((response) => {
+    const imgUrlArray: string[] = response.data['travelImages'];
+    const imgPanelDiv: HTMLElement | null = document.getElementById('home-page-img-div')
+    if (imgPanelDiv) {
+      imgPanelDiv.innerHTML = '';
+      imgUrlArray.forEach((imgUrl) => {
+        imgPanelDiv.innerHTML += addHomePageImage(imgUrl);
+      });
+    } else {
+      throw Error('Could not find div by id: "home-page-img-div"')
+    }
+  })
+}, [config.backend, addHomePageImage]);
