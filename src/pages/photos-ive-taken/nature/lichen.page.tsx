33d3dcfb8d61d778ab3295c1ad72e3d9105a25de
Rob <rob_452@hotmail.co.uk>
2022-10-29 10:48:51 +0100

    Nature photos - progress with s3 image hook

--- src/pages/photos-ive-taken/nature/lichen.page.tsx
+++ src/pages/photos-ive-taken/nature/lichen.page.tsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import InfinitySpinner from '../../../resources/infinity-spinner.svg';
 import '../../page.css';
+import { useGetS3ImageUrls } from './hooks/useGetS3ImageUrls.hook';
 
 export function LichenPage() {
+  const getLichenImageUrls = useGetS3ImageUrls(
+    '/photos-ive-taken/nature/lichen',
+    'lichen-page-img-div'
+  );
+
+  useEffect(() => {
+    getLichenImageUrls();
+  }, []);
+  
   return (
     <div className="standard-page-margins standard-page-styling">
       <h1>
         Lichen
       </h1>
-      <p>
-        Coming soon.
-      </p>
+      <div id="lichen-page-img-div">
+        <h2>Loading images from S3...</h2>
+        <img src={InfinitySpinner}/>
+      </div>
     </div>
   );
 }
