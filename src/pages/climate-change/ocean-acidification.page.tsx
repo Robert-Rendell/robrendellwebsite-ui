4c7319562483f16219ce5ace064f08501ee10e61
Rob <rob_452@hotmail.co.uk>
2022-10-30 10:59:20 +0000

    Climate change - added ocean acidification video and renewable energy video

--- src/pages/climate-change/ocean-acidification.page.tsx
+++ src/pages/climate-change/ocean-acidification.page.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import { useWindowSize } from '../../hooks/use-window-size.hook';
 import '../page.css';
 
 export function OceanAcidification() {
+  const windowSize = useWindowSize();
+  const width = windowSize[0] - 40;
+  const height = windowSize[1]-150;
+
   return (
     <div className="standard-page-margins standard-page-styling">
       <h1>
-        Ocean Acidity
+        Ocean Acidification
       </h1>
-      <p>
-        Content coming soon.
-      </p>
+      <iframe width={width} height={height} src="https://www.youtube.com/embed/L2bxwnm7JG4" 
+        title="YouTube video player" frameBorder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+        allowFullScreen></iframe>
     </div>
   );
 }
