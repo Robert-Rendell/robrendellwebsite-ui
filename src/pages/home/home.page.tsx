17de84b9e520a7486a3f99a65d34091e35d6b4bb
Rob <rob_452@hotmail.co.uk>
2022-10-31 09:42:45 +0000

    Added outage message temporarily

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -31,6 +31,10 @@ export const HomePage = () => {
     <div className="standard-page-margins standard-page-styling">
       <h2 className="centred">Welcome to rob-rendell.co.uk</h2>
       <h3>Rob Rendell: Node.js / TypeScript Developer</h3>
+      <hr/>
+      <h1 style={{color: 'red'}}>Experiencing a problem with CORS / AWS S3 at the moment.</h1>
+      <h3 style={{color: 'yellow'}}>I&apos;ll fix it when I get a chance this week</h3>
+      <hr/>
       <h4>Recent developments:</h4>
       <ul>
         <li>30/10/22 @ 10.33 - Completed <a href="photos-ive-taken/nature/wild-flowers">My Flower Photographs</a> project (phase 1)</li>
