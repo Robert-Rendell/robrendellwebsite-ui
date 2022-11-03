3be42e8b46fdb2ce22a6d239be901dbfb0383a50
Rob <rob_452@hotmail.co.uk>
2022-11-03 08:12:30 +0000

    India - added indian beliefs from 2011 Census

--- src/pages/misc/india.page.tsx
+++ src/pages/misc/india.page.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
 import { useWindowSize } from '../../hooks/use-window-size.hook';
+import { IndianBeliefsPieChart } from './components/indian-beliefs.component';
 
 export function IndiaPage() {
   const windowSize = useWindowSize();
   const width = windowSize[0] - 40;
-  return (<div className="standard-page-margins standard-page-styling">
+  return (<div>
+    <HighchartsReact
+      highcharts={Highcharts}
+      options={IndianBeliefsPieChart}
+    />
     <img width={width} src="https://img.freepik.com/premium-vector/high-quality-labeled-map-india-with-borders-regions_97886-10486.jpg?w=900"/>
     <img width={width} src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Language_region_maps_of_India.svg/1024px-Language_region_maps_of_India.svg.png" />
   </div>);
