edcb8aad75cf4749143b34d1e7b3c2fa290bfbc7
Rob <rob_452@hotmail.co.uk>
2022-11-03 08:20:23 +0000

    India - formatting

--- src/pages/misc/india.page.tsx
+++ src/pages/misc/india.page.tsx
@@ -3,16 +3,27 @@ import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
 import { useWindowSize } from '../../hooks/use-window-size.hook';
 import { IndianBeliefsPieChart } from './components/indian-beliefs.component';
+import './india.page.css';
 
 export function IndiaPage() {
   const windowSize = useWindowSize();
   const width = windowSize[0] - 40;
   return (<div>
+    <h1>India</h1>
+    <hr/>
+    <h2>* 2nd Largest Country by Population *</h2>
+    <hr/>
+    <h2>* Home of the Oldest Belief System: Hinduism *</h2>
+    <hr/>
     <HighchartsReact
       highcharts={Highcharts}
       options={IndianBeliefsPieChart}
     />
+    <hr/>
+    <h1>Indian Regions</h1>
     <img width={width} src="https://img.freepik.com/premium-vector/high-quality-labeled-map-india-with-borders-regions_97886-10486.jpg?w=900"/>
+    <hr/>
+    <h1>Indian Languages</h1>
     <img width={width} src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Language_region_maps_of_India.svg/1024px-Language_region_maps_of_India.svg.png" />
   </div>);
 }
