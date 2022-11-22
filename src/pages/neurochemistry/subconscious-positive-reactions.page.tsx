9929527c0f11843fe9935e7c16e4933b1644e943
Rob <rob_452@hotmail.co.uk>
2022-11-22 13:31:25 +0000

    Neurochemistry - progress on subconscious positive reactions

--- src/pages/neurochemistry/subconscious-positive-reactions.page.tsx
+++ src/pages/neurochemistry/subconscious-positive-reactions.page.tsx
@@ -3,25 +3,67 @@ import { RobBackButton } from "../../components/back-button.component";
 
 export function SubconsciousPositiveReactionsPage() {
   return (
-    <div className="standard-page-margins standard-page-styling black-bg">
+    <div className="standard-page-margins standard-page-styling black-bg-auto black-bg">
       <h1>
         <RobBackButton /> Subconscious Positive Reactions
       </h1>
       <hr/>
       <h2 className="centred">
         <i>
-        Heaven and hell: are they places of the afterlife?
+        Heaven and hell... are they places of the afterlife?
         </i>
       </h2>
       <hr/>
-      <p><b>No</b>, they are places on earth.</p>
+      <p className="centred"><b>No</b> -- they are places on earth.</p>
       <p>
-        I have experienced both and I believe it is down to your state of mind, your interpretation of reality and your reaction to events.
+        I have experienced both, in life, and I believe it is a result of your <u>current state of mind</u>, your <u>interpretation of reality</u> and your <u>reaction</u> to events.
+      </p>
+      <hr/>
+      <h2 className="centred">
+        <i>
+        What does &apos;State of Mind&apos; actually mean?
+        </i>
+      </h2>
+      <hr/>
+      <p>
+        State of mind, in my opinion, is the current (metaphorical) shape of your unconscious mind (subconscious).
       </p>
       <p>
-        I believe when the world feels upside down, you have trained a negative subconscious reaction
-        to the world.
+        Our brains are programs that are constantly being reworked by experience.
       </p>
+      <p>
+        Inputs arrive from the outside and the current state of the program (your mind) determines the output.
+      </p>
+      <p>
+        The outputs, of your mind, are reactions to situations and events.
+      </p>
+      <hr/>
+      <p>
+        Crucially, I believe when the world feels upside down, it is a direct result of your mind being conditioned
+        with a negative outlook. I call this <u>&apos;Subconscious Negative Reactions&apos;</u>.
+      </p>
+      <hr/>
+      <p>
+        So now you know the basics, let us go through a few strategies for a more optimistic outlook:
+      </p>
+      <ol>
+        <li>Take control of your reactions;</li>
+        <li>Actively and deliberately react positively to events, even if it is very difficult.</li>
+      </ol>
+      <p>Practical routes:</p>
+      <ul>
+        <li>&apos;Have a good day!&apos; - When you are finished in a dialog with another person, say &apos;Have a good day!&apos;;</li>
+        <li>Remember to smile!;</li>
+        <li>Write down anything that made you happy today (eg. my lunch was tasty, I enjoyed my tea, getting outside was nice);</li>
+        <li>Spend 5 mins with your eyes closed in a quiet space just imagining the sun on your face on a hot sunny holiday in Spain;</li>
+        <li>Be grateful for: running clean water, shelter, for food, for your hands, for your legs;</li>
+        <li>Think about your favourite things;</li>
+        <li>Imagine cuddling dogs or cats.</li>
+      </ul>
+      <hr/>
+      <p>Most importantly, you need to do these things regularly to build habits to train your subconscious.</p>
+      <p>Human memory is shit, that is one reason why we need computers. Do not rely on your memory.</p>
+      <p>Write notes for yourself, so you are reminded.</p>
     </div>
   );
 }
