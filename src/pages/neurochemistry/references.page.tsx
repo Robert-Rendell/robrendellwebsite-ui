c009d02f344830b44443c11d45a86e1990ad45e3
Rob <rob_452@hotmail.co.uk>
2022-11-23 12:24:42 +0000

    Neurochemistry - adding references

--- src/pages/neurochemistry/references.page.tsx
+++ src/pages/neurochemistry/references.page.tsx
@@ -9,11 +9,42 @@ export function NeurochemistryReferencesPage() {
       </h1>
       <ol>
         <li>
-            Advice from The Beatles: https://i.redd.it/ob5eruy3kfe41.jpg
+          Advice from The Beatles: https://i.redd.it/ob5eruy3kfe41.jpg
         </li>
         <li>
-            The Beatles - Let it Be: https://www.youtube.com/embed/QDYfEBY9NM4
+          The Beatles - Let it Be: https://www.youtube.com/embed/QDYfEBY9NM4
         </li>
+        <li>
+          Self Actualisation: https://www.simplypsychology.org/self-actualization
+        </li>
+        <li>
+          The Athlete&apos;s Way: https://www.amazon.co.uk/Athletes-Way-Sweat-Biology-Bliss/dp/0312355866
+        </li>
+        <li>
+          Brain picture: https://www.hopkinsmedicine.org/-/media/images/health/1_-conditions/brain/brain-main-parts.ashx
+        </li>
+        <li>
+          The Neurochemicals of Happiness: https://www.psychologytoday.com/gb/blog/the-athletes-way/201211/the-neurochemicals-happiness
+        </li>
+        <li>
+          
+        </li>
+        <li>
+          
+        </li>
+        <li>
+          
+        </li>
+        <li>
+          
+        </li>
+        <li>
+          
+        </li>
+        <li>
+          
+        </li>
+
       </ol>
     </div>
   );
