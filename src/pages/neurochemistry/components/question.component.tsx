f5d21aa385b608f8e1feb11441872c39a2d9c051
Rob <rob_452@hotmail.co.uk>
2022-11-23 07:00:35 +0000

    Neurochemistry - added learning and genetic code

---
+++ src/pages/neurochemistry/components/question.component.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+type Props = {
+    questionText: string;
+}
+
+export function QuestionComponent(props: Props) {
+  return (<>
+    <hr />
+    <h2 className="centred">
+      <i>{props.questionText}</i>
+    </h2>
+    <hr />
+  </>
+  );
+}
