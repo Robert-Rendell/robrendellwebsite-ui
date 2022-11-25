cd48483f3a6e166c328d19cfff902aaae170892d
Rob <rob_452@hotmail.co.uk>
2022-11-25 18:15:06 +0000

    Recursion - fix parenthesis

--- src/pages/recursion/simple-equations.page.tsx
+++ src/pages/recursion/simple-equations.page.tsx
@@ -51,8 +51,8 @@ export function SimpleEquationsInfiniteComplexity() {
             is far too similar to the way in which patterns emerge in our universe.
         </p>
         <p>
-            Another interesting point on Conway&apos;s Game of Life, is that in some implementations, the game reaches an
-            equalibrium, where the state stands still. This, of course, may be deliberate in some implementations.
+            Another interesting point on Conway&apos;s Game of Life is that in some implementations, the game reaches an
+            equalibrium where the state stands still. This, of course, may be deliberate in some implementations.
         </p>
         <p>
             However, is it worth contemplating that our Earth reached a point of equalibrium until humans began to overpopulate Earth?
