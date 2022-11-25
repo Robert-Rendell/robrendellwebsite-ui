0c08a4ad27350707600afda42f28b142e492851d
Rob <rob_452@hotmail.co.uk>
2022-11-25 17:42:41 +0000

    Recursion - continued work on simple equations

--- src/pages/recursion/simple-equations.page.tsx
+++ src/pages/recursion/simple-equations.page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { SharedText } from "../../common/shared-text";
 import { NewTabLink } from "../../components/new-tab-link.component";
+import { QuestionComponent } from "../neurochemistry/components/question.component";
 
 export function SimpleEquationsInfiniteComplexity() {
   return (
@@ -22,6 +23,29 @@ export function SimpleEquationsInfiniteComplexity() {
         </p>
         <h2>x<sub>n+1</sub> = rx<sub>n</sub>(1 - x<sub>n</sub>)</h2>
         <hr/>
+        <p>
+            The rules of Conway&apos;s Game of Life are, strangely, also very simple.
+        </p>
+        <ol>
+          <li>
+            Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+          </li>
+          <li>
+            Any live cell with two or three live neighbours lives on to the next generation.
+          </li>
+          <li>
+            Any live cell with more than three live neighbours dies, as if by overpopulation.
+          </li>
+          <li>
+            Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+          </li>
+        </ol>
+        <p>
+          <NewTabLink href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life#Rules">
+            Wikipedia: Conway&apos;s Game of Life
+          </NewTabLink>
+        </p>
+        <QuestionComponent questionText="Is it worth considering, that the equation that governs humans is also very simple?"></QuestionComponent>
       </div>
     </>
   );
