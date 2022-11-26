cd125a5289f2c041e666eee420f05f12c3e797ad
Rob <rob_452@hotmail.co.uk>
2022-11-26 09:44:06 +0000

    Recursion - finished simple equations = infinite complexity page

--- src/pages/neurochemistry/components/question.component.tsx
+++ src/pages/neurochemistry/components/question.component.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 
 type Props = {
-  questionText: string;
+  questionText?: string;
 };
 
-export function QuestionComponent(props: Props) {
+export function QuestionComponent(props: React.PropsWithChildren<Props>) {
   return (
     <>
       <hr />
       <h2 className="centred">
-        <i>{props.questionText}</i>
+        <i>{props.questionText ?? props.children}</i>
       </h2>
       <hr />
     </>
