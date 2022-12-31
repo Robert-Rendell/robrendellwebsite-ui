9e23f9f677950a7d0bee001f40755c97f3fde264
Rob <rob_452@hotmail.co.uk>
2022-12-31 01:38:30 +0000

    Biomagnetism - more content on endocrine

--- src/pages/biomagnetism/human-endocrine-system.page.tsx
+++ src/pages/biomagnetism/human-endocrine-system.page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { SharedRoutes } from "../../common/shared-routes";
 import { SharedText } from "../../common/shared-text";
 import { RobBackButton } from "../../components/back-button.component";
 import { MusicScrollDownMessage } from "../../components/music-scroll-down.component";
@@ -16,7 +17,7 @@ export function BiomagnetismInHumanEndocrineSystemPage() {
         <RobBackButton />{" "}
         {SharedText.Biomagnetism.MagnetismInTheHumanEndocrineSystem}
       </h1>
-      <MusicScrollDownMessage youtubeVideoEmbedId="F0NraYVUuvw">
+      <MusicScrollDownMessage youtubeVideoEmbedId="BSy6b-mg7vQ">
         <QuestionComponent hrOff>
           What connects the{" "}
           <NewTabLink href="https://en.wikipedia.org/wiki/Eye_of_Horus">
@@ -43,14 +44,22 @@ export function BiomagnetismInHumanEndocrineSystemPage() {
           survival to adapt to changing environments.
         </p>
         <p>
-          I see a pattern that has emerged from thousands of years of religion
-          and ancient egyptian hieroglyphs.
+          Many people see a pattern that has emerged from thousands of years of
+          religion and ancient egyptian hieroglyphs.
+        </p>
+        <p>
+          There has been, however, a&nbsp;
+          <NewTabLink href="https://www.researchgate.net/publication/320818682_NN_-dimethyltryptamine_and_the_pineal_gland_Separating_fact_from_myth">
+            journal entry
+          </NewTabLink>
+          &nbsp;written to separate science from myth. I am citing this as to
+          write in a balanced manner.
         </p>
         <QuestionComponent>
           When does the pattern first emerge?
         </QuestionComponent>
         <p>
-          My journey of joining the dots begins with human&apos;s ability to
+          This journey of joining the dots begins with human&apos;s ability to
           imagine.
         </p>
         <p>
@@ -145,17 +154,136 @@ export function BiomagnetismInHumanEndocrineSystemPage() {
           René Descartes believed the human pineal gland to be the
           &quot;principal seat of the soul&quot;.
         </p>
-        <QuestionComponent>
-          What is Melatonin?
-        </QuestionComponent>
+        <QuestionComponent>What is melatonin?</QuestionComponent>
+        <p>
+          Melatonin was discovered in 1958. It is primarily known in animals as
+          a hormone released by the pineal gland in the brain at night and has
+          long been associated with control of the sleep–wake cycle.
+        </p>
         <hr />
-        <p>It</p>
+        <p>
+          In vertebrates, melatonin is involved in synchronizing circadian
+          rhythms, including sleep–wake timing and blood pressure regulation,
+          and in control of seasonal rhythmicity including reproduction,
+          fattening, moulting and hibernation.
+        </p>
+        <p>
+          Many of its effects are through activation of the melatonin receptors,
+          while others are due to its role as an antioxidant. In plants, it
+          functions to defend against oxidative stress. It is also present in
+          various foods.
+        </p>
+        <hr />
+        <p>
+          See my other work in this area:&nbsp;
+          <NewTabLink
+            href={SharedRoutes.Neurochemistry.MelatoninIsTheKeyToCreativity}
+          >
+            {SharedText.Neurochemistry.MelatoninIsTheKeyToCreativity}
+          </NewTabLink>
+        </p>
+        <p>
+          I personally believe the pineal gland is crucial in creative insight
+          and I have experienced epiphany first hand on multiple occasions.
+        </p>
+        <p>
+          I believe the epiphysis cerebri (pineal gland) plays a fundamental
+          role in the mechanics of epiphany.
+        </p>
+        <p>
+          When your eyes are exposed to darkness, I believe the melatonin
+          secretion signals a significant boost in your mind&apos;s eye sight.
+        </p>
+        <p>
+          Your visual inputs (your eyes) use a huge amount of energy but when
+          you are in a pitch black room, your body can easily switch off this
+          expensive sense which, in turn, allows more energy to be redirected to
+          your imagination, your third eye and your inner projections.
+        </p>
+        <p>
+          This effect could be dampened by inner injections of adrenaline since
+          the adrenal glands are part of the same endocrine system as melatonin.
+        </p>
+        <p>
+          I also consider the possibility that increased adrenaline in total
+          darkness due to fear could heighten your imagination / creative
+          insight but in an uncontrollable emotional way. The experience might
+          not be as insightful because your emotions begin to override any
+          imagination processes in the up brain.
+        </p>
+        <QuestionComponent>
+          Fascinating... so what does this have to do with{" "}
+          <NewTabLink href="https://en.wikipedia.org/wiki/Ayahuasca">
+            ayahuasca
+          </NewTabLink>{" "}
+          (Dimethyltryptamine)?
+        </QuestionComponent>
+        <p>
+          Dimethyltryptamine has been found to be secreted by the pineal gland
+          too.
+        </p>
+        <div className="centred">
+          <img
+            width={width / 3}
+            src="https://m.media-amazon.com/images/I/5171wSAYylL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg"
+          />
+        </div>
+        <p>
+          In this book, the following molecular chemicals are shown to fit into serotonin receptors because of their similar shape.
+        </p>
+        <p>
+          The idea presented is that, each chemical fits into the serotonin receptors like different keys that can all unlock the
+          serotonin receptor lock, however each lifts different &quot;tumblers&quot; when opening the lock.
+        </p>
+        <div style={{ width: "100%", background: "white" }}>
+          <img
+            width={width / 4}
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c4/Serotonin-2D-skeletal.svg/1280px-Serotonin-2D-skeletal.svg.png"
+          />
+          <img
+            width={width / 4}
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/Melatonin.svg/1280px-Melatonin.svg.png"
+          />
+          <img
+            width={width / 4}
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/DMT.svg/1920px-DMT.svg.png"
+          />
+          <img
+            width={width / 4}
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Psilocybn.svg/1280px-Psilocybn.svg.png"
+          />
+        </div>
+        <div id="neurotransmitter-table">
+          <table width={width}>
+            <tr>
+              <td style={{ textAlign: "center" }}>Serotonin</td>
+              <td>Melatonin</td>
+              <td>Dimethyltryptamine (DMT)</td>
+              <td>Psilocybin</td>
+            </tr>
+          </table>
+        </div>
+        <p></p>
+        <QuestionComponent>
+          And how does this all link to magnetism?
+        </QuestionComponent>
+        <p>
+          In the last 20 years, researchers in Grenoble have found calcite
+          microcrystals present in the pineal gland.
+        </p>
+        <p>
+          There is a possibility that this type of material could be capable
+          of&nbsp;
+          <NewTabLink href="https://en.wikipedia.org/wiki/Piezoelectricity">
+            Piezoelectricity
+          </NewTabLink>
+          .
+        </p>
         <p></p>
         <p>https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6088236/ -dmt</p>
         <p>
           https://revibeenergy.com/piezoelectric-electromagnetic-harvesting/
         </p>
-        <p>https://en.wikipedia.org/wiki/Piezoelectricity</p>
         <p>https://pubmed.ncbi.nlm.nih.gov/12224052/ 2002</p>
         <p>https://pubmed.ncbi.nlm.nih.gov/21894451/ 2012</p>
         <p>https://pubmed.ncbi.nlm.nih.gov/21544842/ 2011</p>
