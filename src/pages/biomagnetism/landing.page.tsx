2205ff4b9ac1c27fc6bcb3e2eb0a3ddb018cdf62
Rob <rob_452@hotmail.co.uk>
2023-01-07 13:20:04 +0000

    Fix some grammar and linting

--- src/pages/biomagnetism/landing.page.tsx
+++ src/pages/biomagnetism/landing.page.tsx
@@ -3,12 +3,9 @@ import { SharedRoutes } from "../../common/shared-routes";
 import { SharedText } from "../../common/shared-text";
 import { MusicScrollDownMessage } from "../../components/music-scroll-down.component";
 import { NewTabLink } from "../../components/new-tab-link.component";
-import { useWindowSize } from "../../hooks/use-window-size.hook";
 import "../page.css";
 
 export function BiomagnetismLandingPage() {
-  const windowSize = useWindowSize();
-  const width = windowSize[0] - 40;
   return (
     <div className="standard-page-margins standard-page-styling">
       <h1>{SharedText.Biomagnetism.LandingPage}</h1>
