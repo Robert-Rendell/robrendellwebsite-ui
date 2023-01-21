e48736e09544ff097de82c2e16717688575fd4dc
Rob <rob_452@hotmail.co.uk>
2023-01-21 12:23:15 +0000

    Footer - add tribute to Northumberland and Scotland

--- src/components/rob-rendell-footer.component.tsx
+++ src/components/rob-rendell-footer.component.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { useWindowSize } from "../hooks/use-window-size.hook";
+import Northumberland from "../resources/flag-of-northumberland.svg";
+import Saltire from "../resources/saltire.svg";
 
 export function RobRendellFooterComponent() {
+  const windowSize = useWindowSize();
+  const width = windowSize[0] - 40;
   return (
     <>
       <div className="centred">
+        <img src={Northumberland} width={width/20} style={{margin: "10px", float:"left"}}/>
+        <img src={Saltire} width={width/20} style={{margin: "10px", float:"right"}}/>
         <span style={{ color: "white", textAlign: "center" }}>
           Copyright &copy; 2021-2023 Robert Rendell. All rights reserved.
         </span>
