83e50145feb42109478e966809779528e6a05674
Rob <rob_452@hotmail.co.uk>
2023-01-29 14:36:23 +0000

    Fix lint

--- src/pages/misc/philosophy.page.tsx
+++ src/pages/misc/philosophy.page.tsx
@@ -2,11 +2,8 @@ import React from "react";
 import { SharedRoutes } from "../../common/shared-routes";
 import { SharedText } from "../../common/shared-text";
 import { usePageView } from "../../hooks/use-page-view.hook";
-import { useWindowSize } from "../../hooks/use-window-size.hook";
 
 export function PhilosophyPage() {
-  const windowSize = useWindowSize();
-  const width = windowSize[0] - 40;
   usePageView(SharedRoutes.Miscellaneous.Philosophy);
 
   return (
