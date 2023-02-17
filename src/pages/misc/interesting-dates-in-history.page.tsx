764c051d22d67013702277478eff7f2eefe41fe6
Rob <rjprendell@gmail.com>
2023-02-17 12:01:09 +0000

    Misc - add spinner to dates in history

--- src/pages/misc/interesting-dates-in-history.page.tsx
+++ src/pages/misc/interesting-dates-in-history.page.tsx
@@ -4,6 +4,7 @@ import { Button, Table } from "react-bootstrap";
 import { InterestingDateInHistory } from "robrendellwebsite-common";
 import { SharedRoutes } from "../../common/shared-routes";
 import { SharedText } from "../../common/shared-text";
+import { InfinitySpinnerComponent } from "../../components/infinity-spinner.component";
 import { usePageView } from "../../hooks/use-page-view.hook";
 import { useWindowSize } from "../../hooks/use-window-size.hook";
 import { useDatesInHistory } from "./hooks/useDatesInHistory.hook";
@@ -28,72 +29,82 @@ export function InterestingDatesInHistoryPage() {
   return (
     <div className="standard-page-margins standard-page-styling">
       <h1>{SharedText.Miscellaneous.InterestingDatesInHistory}</h1>
-      <select
-        onChange={(ev) => {
-          setFilter({ school: ev.target.value });
-        }}
-      >
-        <option label={selectASchool} value={selectASchool}></option>
-        {schools?.map((s) => (
-          <>
-            <option label={s} value={s}></option>
-          </>
-        ))}
-      </select>
-      <Table variant="dark">
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Event</th>
-            <th>School</th>
-          </tr>
-        </thead>
-        <tbody>
-          {datesInHistory?.map((dateInHistory, index) => {
-            const shouldShow =
-              typeof filter === "undefined" ||
-              filter.school === selectASchool ||
-              filter.school?.toUpperCase() ===
-                dateInHistory.school.trim().toUpperCase();
-            return (
-              shouldShow && (
-                <tr key={index}>
-                  <td onClick={() => alert("Index: " + index)}>
-                    {dateInHistory.date}
-                  </td>
+      {!datesInHistory ? (
+        <InfinitySpinnerComponent />
+      ) : (
+        <>
+          <select
+            onChange={(ev) => {
+              setFilter({ school: ev.target.value });
+            }}
+          >
+            <option label={selectASchool} value={selectASchool}></option>
+            {schools?.map((s) => (
+              <>
+                <option label={s} value={s}></option>
+              </>
+            ))}
+          </select>
+          <Table variant="dark">
+            <thead>
+              <tr>
+                <th>Date</th>
+                <th>Event</th>
+                <th>School</th>
+              </tr>
+            </thead>
+            <tbody>
+              {datesInHistory?.map((dateInHistory, index) => {
+                const shouldShow =
+                  typeof filter === "undefined" ||
+                  filter.school === selectASchool ||
+                  filter.school?.toUpperCase() ===
+                    dateInHistory.school.trim().toUpperCase();
+                return (
+                  shouldShow && (
+                    <tr key={index}>
+                      <td onClick={() => alert("Index: " + index)}>
+                        {dateInHistory.date}
+                      </td>
 
-                  <td>
-                    {dateInHistory.event.length < maxLengthOnMobile ||
-                    expandedDateIndex === index ||
-                    !onMobile ? (
-                      dateInHistory.event
-                    ) : (
-                      <>
-                        {dateInHistory.event.substring(0, maxLengthOnMobile)}{" "}
-                        <Button
-                          size="sm"
-                          onClick={() => setExpandedDateIndex(index)}
-                        >
-                          ...
-                        </Button>
-                      </>
-                    )}
-                    {expandedDateIndex === index && (
-                      <Button
-                        size="sm"
-                        onClick={() => setExpandedDateIndex(undefined)}
-                      >
-                        x
-                      </Button>
-                    )}
-                  </td>
-                  <td>{dateInHistory.school}</td>
-                </tr>
-              )
-            );
-          })}
-        </tbody>
-      </Table>
+                      <td>
+                        {dateInHistory.event.length < maxLengthOnMobile ||
+                        expandedDateIndex === index ||
+                        !onMobile ? (
+                          dateInHistory.event
+                        ) : (
+                          <>
+                            {dateInHistory.event.substring(
+                              0,
+                              maxLengthOnMobile
+                            )}{" "}
+                            <Button
+                              size="sm"
+                              onClick={() => setExpandedDateIndex(index)}
+                            >
+                              ...
+                            </Button>
+                          </>
+                        )}
+                        {expandedDateIndex === index && (
+                          <Button
+                            size="sm"
+                            onClick={() => setExpandedDateIndex(undefined)}
+                          >
+                            x
+                          </Button>
+                        )}
+                      </td>
+                      <td>{dateInHistory.school}</td>
+                    </tr>
+                  )
+                );
+              })}
+            </tbody>
+          </Table>
+        </>
+      )}
+      ;
     </div>
   );
 }
