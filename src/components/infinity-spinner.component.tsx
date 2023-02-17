b572ba11b8ecd9791dd97570a159e9e0daacba7a
Rob <rjprendell@gmail.com>
2023-02-17 11:59:35 +0000

    Misc - add spinner to word of the day

---
+++ src/components/infinity-spinner.component.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import InfinitySpinner from "../resources/infinity-spinner.svg";
+export function InfinitySpinnerComponent() {
+  return <img src={InfinitySpinner} />;
+}
