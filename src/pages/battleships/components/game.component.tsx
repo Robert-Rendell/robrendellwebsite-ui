6490a7cbee79f4f86f79b60b46b16d9a428a194b
Rob <rjprendell@gmail.com>
2023-03-04 16:07:37 +0000

    Battleships: added a game board when state allows it

--- src/pages/battleships/components/game.component.tsx
+++ src/pages/battleships/components/game.component.tsx
@@ -1,17 +1,49 @@
 import React from "react";
 import { BattleshipsGame } from "robrendellwebsite-common";
+import "./game.component.css";
 
 type Props = {
   game?: BattleshipsGame;
 };
 export function BattleshipsGameComponent(props: Props) {
+  const [rows, cols] = props.game?.boardDimensions || [0, 0];
+  const state = props.game?.state;
   return (
     <>
       {!props.game && <h2>No game loaded.</h2>}
       {props.game && (
         <>
-          <h2>Game ID: [{props.game.gameId}]</h2>
-          <p>{JSON.stringify(props.game)}</p>
+          <>
+            <h2>Game ID: [{props.game.gameId}]</h2>
+            <p>{JSON.stringify(props.game)}</p>
+          </>
+          <hr />
+          <p>{state === "created" && <p>Waiting for opponent to join...</p>}</p>
+          <>
+            {state === "configuring" ||
+              (state === "playing" && (
+                <table className="battleships-table">
+                  <tbody>
+                    {Array.from(Array(rows)).map((x, i) => {
+                      console.log(x, i);
+                      return (
+                        <tr key={`${x}-${i}`}>
+                          {Array.from(Array(cols)).map((y, j) => {
+                            console.log(y, i);
+                            return (
+                              <td
+                                className="battleships-td"
+                                key={`${y}-${j}`}
+                              ></td>
+                            );
+                          })}
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </table>
+              ))}
+          </>
         </>
       )}
     </>
