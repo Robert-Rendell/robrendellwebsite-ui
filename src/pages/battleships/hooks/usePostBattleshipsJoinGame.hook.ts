261bdad3e33fc9653df47b29df826c48f3a94b68
Rob <rjprendell@gmail.com>
2023-03-04 12:56:50 +0000

    Fix lint

--- src/pages/battleships/hooks/usePostBattleshipsJoinGame.hook.ts
+++ src/pages/battleships/hooks/usePostBattleshipsJoinGame.hook.ts
@@ -3,7 +3,6 @@ import { useEffect, useState } from "react";
 import {
   BattleshipsErrorResponse,
   BattleshipsGame,
-  GetBattleshipsGameResponse,
   PostBattleshipsJoinGameRequest,
   PostBattleshipsJoinGameResponse,
 } from "robrendellwebsite-common";
